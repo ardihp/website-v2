a8c395fe473bdb3a1334d6ec7ebdcfe12174d1dc
ardihp <ardihp8@gmail.com>
2024-07-09 14:51:24 +0700

    feat: custom cursor

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,13 +1,59 @@
-import React, { ReactNode } from "react";
+"use client";
+
+import React, { ReactNode, useEffect } from "react";
 import HeaderSection from "./header";
 import FooterSection from "./footer";
+import { IconCircleFilled, IconEye, IconEyeFilled } from "@tabler/icons-react";
 
 export default function LayoutSection({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    const moveCursor = (e: any) => {
+      const cursor: any =
+        document.getElementsByClassName("cursor")?.item(0) || "";
+      const cursorOverlay: any =
+        document.getElementsByClassName("cursor-overlay")?.item(0) || "";
+
+      const button = Array.from(document.querySelectorAll("button"));
+
+      button.forEach((btn) => {
+        btn.addEventListener("mousemove", function () {
+          cursor.classList.add("hover-cursor");
+          cursorOverlay.classList.add("hover-cursor");
+        });
+        btn.addEventListener("mouseleave", function () {
+          cursor.classList.remove("hover-cursor");
+          cursorOverlay.classList.remove("hover-cursor");
+        });
+      });
+
+      const mouseY = e.clientY;
+      const mouseX = e.clientX;
+
+      cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+      cursorOverlay.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+
+    return () => {
+      window.removeEventListener("mouseover", moveCursor);
+    };
+  }, []);
+
   return (
-    <div className="flex flex-col min-h-screen">
-      <HeaderSection />
-      <div className="w-full h-full flex flex-col flex-grow">{children}</div>
-      <FooterSection />
-    </div>
+    <>
+      <div className="absolute flex items-center justify-center">
+        <div className="cursor">
+          <IconCircleFilled className="dark:text-white text-black w-[8px] h-[8px] icon-dot" />
+          <IconEyeFilled className="hidden dark:text-white text-black w-[14px] h-[14px] icon-eye" />
+        </div>
+        <div className="cursor-overlay" />
+      </div>
+      <div className="flex flex-col min-h-screen">
+        <HeaderSection />
+        <div className="w-full h-full flex flex-col flex-grow">{children}</div>
+        <FooterSection />
+      </div>
+    </>
   );
 }
