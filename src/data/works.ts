ac828fc3259fb0610ef11cc3b4dc0bc7c0839ff7
ardihp <ardihp8@gmail.com>
2024-08-02 16:54:05 +0700

    fix(works): configure list works

--- src/data/works.ts
+++ src/data/works.ts
@@ -1,44 +1,59 @@
-export const listWorks = [
+interface Work {
+  company: string;
+  desc: string;
+  image: string;
+  tech: string[];
+  live: string;
+}
+
+export const listWorks: Work[] = [
   {
     company: "In You Market",
-    desc: "",
-    image: "",
+    desc: "A market place, for selling organic items",
+    image: "/works/inyou.png",
     tech: ["Next.js", "TypeScript", "TailwindCSS"],
-    live: "",
+    live: "https://inyoumarket.net",
   },
   {
     company: "Es Teh Indonesia",
-    desc: "",
-    image: "",
+    desc: "Internal CRM Dashboard",
+    image: "/works/esteh.jpg",
     tech: ["Next.js", "Firebase", "MUI Material", "Socket.io"],
     live: "",
   },
   {
     company: "Otego Media",
-    desc: "",
-    image: "",
+    desc: "Internal ERP Dashboard",
+    image: "/works/otego.jpg",
     tech: ["Next.js", "MUI Material"],
     live: "",
   },
   {
-    company: "Rakata",
-    desc: "",
-    image: "",
+    company: "Rakata ID",
+    desc: "One stop literary corner for book lovers. Read, write, connect 📖",
+    image: "/works/rakata.jpg",
     tech: ["React.js", "TypeScript", "Firebase", "Styled Components"],
-    live: "",
+    live: "https://rakata.id",
   },
   {
     company: "Blue Karma Secrets",
-    desc: "",
-    image: "",
+    desc: "𝑨 𝒎𝒂𝒈𝒊𝒄𝒂𝒍 𝒄𝒐𝒎𝒃𝒊𝒏𝒂𝒕𝒊𝒐𝒏 𝒐𝒇 𝒍𝒖𝒔𝒉 𝒔𝒆𝒕𝒕𝒊𝒏𝒈𝒔, 𝒘𝒆𝒍𝒍𝒏𝒆𝒔𝒔 𝒆𝒙𝒑𝒆𝒓𝒊𝒆𝒏𝒄𝒆𝒔, 𝒂𝒏𝒅 𝒆𝒙𝒒𝒖𝒊𝒔𝒊𝒕𝒆𝒍𝒚 𝒂𝒑𝒑𝒐𝒊𝒏𝒕𝒆𝒅 𝒅𝒐𝒎𝒊𝒄𝒊𝒍𝒆𝒔",
+    image: "/works/bks.png",
     tech: ["Next.js", "TypeScript", "Mantine UI", "Socket.io"],
-    live: "",
+    live: "https://bluekarmasecrets.com",
   },
   {
     company: "Lucy Dream Art",
-    desc: "",
-    image: "",
+    desc: "Discover the ultimate online destination for purchasing artwork. Explore a diverse range of original paintings, fine art photographs, and more from the most comprehensive selection available",
+    image: "/works/lucydream.jpeg",
     tech: ["Next.js", "TypeScript", "Mantine UI", "Socket.io"],
+    live: "https://lucydreamart.com",
+  },
+  {
+    company: "Poke Render",
+    desc: "Learn the difference between CSR and SSR using data from Poke API",
+    image: "/logo.png",
+    tech: ["Next.js", "TypeScript"],
     live: "",
   },
 ];
--- src/modules/works/view.tsx
+++ src/modules/works/view.tsx
@@ -1,31 +1,86 @@
+import { listWorks } from "@/data/works";
+import { IconCirclesRelation, IconLink } from "@tabler/icons-react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function WorksView() {
   return (
-    <div className="flex flex-col max-w-screen-lg mx-auto w-full pt-[52px]">
+    <div className="flex flex-col max-w-screen-lg mx-auto w-full pt-[48px]">
       <div className="flex flex-col">
-        <div className="grid grid-cols-3 px-[48px]">
-          <div className="flex items-center border-x border-dashed dark:border-zinc-700 border-zinc-300 w-full p-5">
+        <div className="grid grid-cols-[1fr_.8fr_1fr] px-[48px]">
+          <div className="flex items-center border-x border-style w-full p-5">
             <p className="font-fredoka text-2xl font-medium">My Works</p>
           </div>
           <div className="w-full"></div>
-          <div className="border-x border-dashed dark:border-zinc-700 border-zinc-300 w-full"></div>
+          <div className="border-x border-style w-full"></div>
         </div>
         <div className="flex w-full">
-          <div className="w-full max-w-[48px] border-y border-dashed dark:border-zinc-700 border-zinc-300" />
-          <div className="flex items-center border border-dashed dark:border-zinc-700 border-zinc-300 p-5 w-full">
+          <div className="w-full max-w-[48px] border-y border-style" />
+          <div className="flex items-center border border-style p-5 w-full">
             <p className="font-manrope font-medium">
-              Place of all my works that i have done also some projects from the
-              interesting ideas that sometimes come to mind.
+              Place of all my works that i have done including projects from the
+              interesting ideas that accross my mind.
             </p>
           </div>
-          <div className="w-full max-w-[48px] border-y border-dashed dark:border-zinc-700 border-zinc-300" />
+          <div className="w-full max-w-[48px] border-y border-style" />
         </div>
-        <div className="grid grid-cols-3 px-[48px] h-[36px]">
-          <div className="flex items-center border-x border-dashed dark:border-zinc-700 border-zinc-300 w-full"></div>
+        <div className="grid grid-cols-[1fr_.8fr_1fr] px-[48px] h-[36px]">
+          <div className="flex gap-4 items-center border-x border-style w-full"></div>
           <div className="w-full"></div>
-          <div className="border-x border-dashed dark:border-zinc-700 border-zinc-300 w-full"></div>
+          <div className="border-x border-style w-full"></div>
         </div>
+      </div>
+
+      <div className="columns-2 pt-[48px] gap-8 px-[48px] h-full">
+        {listWorks?.map((work, index: number) => (
+          <Link
+            key={index}
+            href={work.live || "#"}
+            className="no-underline cursor-none mb-8 inline-block w-full"
+            passHref
+          >
+            <div className="flex flex-col border border-style group border-solid dark:border-opacity-50 hover:dark:border-opacity-100 p-4 rounded-lg h-fit bg-transparent hover:bg-zinc-900/40 duration-300">
+              <div className="border border-style border-solid p-1 rounded-full w-fit">
+                <div className="relative z-[2] h-[35px] w-[35px] rounded-full overflow-hidden">
+                  <Image
+                    src={work.image}
+                    alt="Logo Workplace"
+                    fill
+                    className="object-cover object-center w-full h-auto"
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col pt-4 gap-2">
+                <p className="font-fredoka font-medium text-xl">
+                  {work.company}
+                </p>
+                <p className="font-manrope text-sm line-clamp-2 text-pretty opacity-80">
+                  {work.desc}
+                </p>
+              </div>
+
+              <div className="flex flex-col justify-center gap-[6px] mt-6">
+                <p className="font-fredoka text-sm opacity-80">
+                  {work.tech?.join(", ")}
+                </p>
+
+                {work.live && (
+                  <div className="flex items-center gap-2 opacity-80">
+                    <IconCirclesRelation
+                      size={16}
+                      className="dark:text-sky-400 text-sky-500"
+                    />
+                    <p className="font-fredoka text-sm dark:text-sky-400 text-sky-500 group-hover:underline">
+                      {work.live}
+                    </p>
+                  </div>
+                )}
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
