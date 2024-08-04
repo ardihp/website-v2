d7a532970f57c86d15fa0a86605252d39db530af
ardihp <ardihp8@gmail.com>
2024-08-04 14:43:53 +0700

    fix(works): implement color & animation

--- src/modules/works/components/work-item.tsx
+++ src/modules/works/components/work-item.tsx
@@ -1,27 +1,28 @@
-"use client";
-
 import React from "react";
 import Link from "next/link";
 import { Work } from "@/data/works";
 import Image from "next/image";
 import { IconCirclesRelation } from "@tabler/icons-react";
 import DelayedItem from "@/components/layouts/components/delayed-item";
+import { motion } from "framer-motion";
 
 interface WorkItemProps {
   work: Work;
   delay?: number;
+  start?: string;
+  end?: string;
 }
 
-export default function WorkItem({ work, delay }: WorkItemProps) {
+export default function WorkItem({ work, delay, start, end }: WorkItemProps) {
   return (
-    <DelayedItem delay={delay}>
+    <DelayedItem delay={delay} start={start} end={end}>
       <Link
         href={work.live || "#"}
-        className="no-underline cursor-none mb-8 inline-block w-full"
+        className="no-underline cursor-none inline-block w-full"
         target={work.live ? "_blank" : "_self"}
         passHref
       >
-        <div className="flex flex-col border border-style group border-solid dark:border-opacity-50 hover:dark:border-opacity-100 p-4 rounded-lg h-fit bg-transparent hover:bg-zinc-900/40 duration-300">
+        <div className="flex flex-col border border-style group border-solid dark:border-opacity-50 hover:dark:border-opacity-100 p-4 rounded-[16px] h-fit hover:dark:bg-zinc-900/40 backdrop-blur-[1px] hover:bg-secondary/[0.02] duration-300">
           <div className="border border-style border-solid p-1 rounded-full w-fit">
             <div className="relative z-[2] h-[35px] w-[35px] rounded-full overflow-hidden">
               <Image
@@ -35,7 +36,7 @@ export default function WorkItem({ work, delay }: WorkItemProps) {
 
           <div className="flex flex-col pt-4 gap-2">
             <p className="font-fredoka font-medium text-xl">{work.company}</p>
-            <p className="font-manrope text-sm line-clamp-2 text-pretty opacity-80">
+            <p className="font-manrope text-sm font-medium line-clamp-2 text-pretty opacity-80">
               {work.desc}
             </p>
           </div>
@@ -49,9 +50,10 @@ export default function WorkItem({ work, delay }: WorkItemProps) {
               <div className="flex items-center gap-2 opacity-80">
                 <IconCirclesRelation
                   size={16}
-                  className="dark:text-sky-400 text-sky-500"
+                  stroke={3}
+                  className="text-tertiary"
                 />
-                <p className="font-fredoka text-sm dark:text-sky-400 text-sky-500 group-hover:underline">
+                <p className="font-fredoka text-sm font-medium text-tertiary group-hover:underline">
                   {work.live}
                 </p>
               </div>
--- src/modules/works/view.tsx
+++ src/modules/works/view.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import WorkItem from "./components/work-item";
 import { listWorks } from "@/data/works";
@@ -6,34 +8,34 @@ import DelayedItem from "@/components/layouts/components/delayed-item";
 export default function WorksView() {
   return (
     <DelayedItem>
-      <div className="flex flex-col max-w-screen-lg mx-auto w-full pt-[48px]">
+      <div className="flex flex-col max-w-screen-lg mx-auto w-full pt-[24px]">
         <div className="flex flex-col">
           <div className="grid grid-cols-[1fr_.8fr_1fr] px-[48px]">
-            <div className="flex items-center border-x border-style w-full p-5">
+            <div className="flex items-center border-x border-style border-secondary/15 w-full p-5">
               <p className="font-fredoka text-2xl font-medium">My Works</p>
             </div>
             <div className="w-full"></div>
-            <div className="border-x border-style w-full"></div>
+            <div className="border-x border-style border-secondary/15 w-full"></div>
           </div>
           <div className="flex w-full">
-            <div className="w-full max-w-[48px] border-y border-style" />
-            <div className="flex items-center border border-style p-5 w-full">
+            <div className="w-full max-w-[48px] border-y border-style border-secondary/15" />
+            <div className="flex items-center border border-style border-secondary/15 p-5 w-full">
               <p className="font-manrope font-medium">
                 Place of all my works that i have done including projects from
                 the interesting ideas that accross my mind.
               </p>
             </div>
-            <div className="w-full max-w-[48px] border-y border-style" />
+            <div className="w-full max-w-[48px] border-y border-style border-secondary/15" />
           </div>
           <div className="grid grid-cols-[1fr_.8fr_1fr] px-[48px] h-[36px]">
-            <div className="flex gap-4 items-center border-x border-style w-full"></div>
+            <div className="flex gap-4 items-center border-x border-style border-secondary/15 w-full"></div>
             <div className="w-full"></div>
-            <div className="border-x border-style w-full"></div>
+            <div className="border-x border-style border-secondary/15 w-full"></div>
           </div>
         </div>
 
-        <div className="grid grid-cols-2 pt-[48px] gap-8 px-[48px] h-full">
-          <div className="flex flex-col w-full">
+        <div className="grid md:grid-cols-2 pt-[48px] gap-9 px-[48px] h-full">
+          <div className="flex flex-col gap-9 w-full">
             {listWorks?.map(
               (work, index: number) =>
                 index % 2 === 0 && (
@@ -41,11 +43,13 @@ export default function WorksView() {
                     key={index}
                     work={work}
                     delay={index < 6 ? 0.2 * index + 0.25 : 0.25}
+                    start="right"
+                    end="right"
                   />
                 )
             )}
           </div>
-          <div className="flex flex-col w-full">
+          <div className="flex flex-col gap-9 w-full">
             {listWorks?.map(
               (work, index: number) =>
                 index % 2 !== 0 && (
@@ -53,6 +57,8 @@ export default function WorksView() {
                     key={index}
                     work={work}
                     delay={index < 6 ? 0.2 * index + 0.25 : 0.25}
+                    start="left"
+                    end="left"
                   />
                 )
             )}
