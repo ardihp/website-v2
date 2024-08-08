c9000e1639f9c913f6c4694f38414af598a28d46
ardihp <ardihp8@gmail.com>
2024-08-08 18:46:09 +0700

    fix(layouts): remove animate presence on index layouts

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,25 +1,15 @@
-"use client";
-
 import React, { ReactNode } from "react";
 import NavbarSection from "./navbar";
 import FooterSection from "./footer";
-import { AnimatePresence } from "framer-motion";
-import { usePathname } from "next/navigation";
 
 export default function LayoutSection({ children }: { children: ReactNode }) {
-  const pathname = usePathname();
-
   return (
-    <AnimatePresence mode="wait">
-      <React.Fragment key={pathname}>
-        <div className="flex flex-col min-h-screen">
-          <NavbarSection />
-          <div className="w-full h-full flex flex-col flex-grow max-w-screen-lg mx-auto backdrop-blur-sm">
-            {children}
-          </div>
-          <FooterSection />
-        </div>
-      </React.Fragment>
-    </AnimatePresence>
+    <div className="flex flex-col min-h-screen">
+      <NavbarSection />
+      <div className="w-full h-full flex flex-col flex-grow max-w-screen-lg mx-auto backdrop-blur-sm">
+        {children}
+      </div>
+      <FooterSection />
+    </div>
   );
 }
