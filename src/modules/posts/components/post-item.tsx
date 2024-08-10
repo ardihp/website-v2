4b8b83c461a3be71aa796aa5101f9172090a9849
ardihp <ardihp8@gmail.com>
2024-08-10 14:34:26 +0700

    fix(posts): post item component

--- src/modules/posts/components/post-item.tsx
+++ src/modules/posts/components/post-item.tsx
@@ -1,4 +1,5 @@
 import DelayedItem from "@/components/layouts/components/delayed-item";
+import { formatDate } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -17,23 +18,23 @@ export default function PostItem({ post, delay, start, end }: PostItemsProps) {
         href={`/posts/${post?.properties?.slug?.rich_text?.[0]?.plain_text}`}
         passHref
       >
-        <div className="flex flex-col h-fit rounded-[16px] w-full group shadow-inner shadow-secondary/10 dark:shadow-zinc-700/80 hover:dark:bg-zinc-900/40 hover:bg-secondary/[0.02] duration-200 relative">
-          <div className="p-4 h-[200px] w-full">
-            <div className="relative h-full w-full shadow shadow-secondary/10 dark:shadow-zinc-700 rounded-t-[14px] overflow-hidden">
-              <Image
-                src={post?.cover?.external?.url}
-                alt="Blog Cover Image"
-                fill
-                sizes="500px"
-                className="object-cover object-bottom"
-              />
-            </div>
+        <div className="flex flex-col gap-2 h-full w-full p-4 rounded-[32px] shadow-inner shadow-secondary/10 dark:shadow-zinc-700 hover:dark:bg-zinc-900/40 backdrop-blur-[1px] hover:bg-secondary/[0.02] duration-200">
+          <div className="relative h-[220px] w-full rounded-[20px] overflow-hidden border border-secondary/15">
+            <Image
+              src={post?.cover?.file?.url || post?.cover?.external?.url}
+              alt="Blog Cover Image"
+              fill
+              sizes="500px"
+              className="object-cover object-center"
+            />
           </div>
-          <div className="flex flex-col p-4 gap-1 rounded-[14px] w-full mt-[-20px] bg-primary shadow-inner shadow-secondary/10 dark:shadow-zinc-700/80 z-[2]">
+          <div className="flex flex-col gap-1 p-2 rounded-[14px] w-full">
             <p className="font-fredoka text-xl font-medium">
-              {post?.properties?.Title?.title?.[0]?.plain_text}
+              {post?.properties?.title?.title?.[0]?.plain_text}
             </p>
-            <p className="font-manrope text-sm">{post?.created_time}</p>
+            <p className="font-manrope text-sm font-medium text-pretty">
+              {post?.properties?.description?.rich_text?.[0]?.plain_text}
+            </p>
           </div>
         </div>
       </Link>
