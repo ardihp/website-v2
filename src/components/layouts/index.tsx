79810f9d122493523ef82a9b7c1bba341d13b6b8
ardihp <ardihp8@gmail.com>
2024-08-10 17:19:14 +0700

    feat(layouts): navbar progress

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import React, { ReactNode } from "react";
 import NavbarSection from "./navbar";
 import FooterSection from "./footer";
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 
 export default function LayoutSection({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
+      <div className="shadow shadow-secondary/10">
+        <ProgressBar
+          height="2px"
+          color="#3f3f46cc"
+          options={{ showSpinner: false, easing: "ease-in-out" }}
+          shallowRouting
+        />
+      </div>
       <NavbarSection />
       <div className="w-full h-full flex flex-col flex-grow max-w-screen-lg mx-auto backdrop-blur-sm">
         {children}
