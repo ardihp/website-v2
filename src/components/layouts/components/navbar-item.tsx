bd33892b6a1674d88f9876a9940a748166eacde6
ardihp <ardihp8@gmail.com>
2024-08-12 22:13:42 +0700

    fix(layouts): navbar item

--- src/components/layouts/components/navbar-item.tsx
+++ src/components/layouts/components/navbar-item.tsx
@@ -18,7 +18,7 @@ export default function NavbarItem({ href, text, ...rest }: NavbarItemProps) {
       {...rest}
       className={`${
         pathname.match(href)
-          ? "opacity-100 shadow-inner shadow-secondary/15 dark:shadow-zinc-700/80"
+          ? "opacity-100 shadow-inner shadow-secondary/15 dark:shadow-zinc-700/80 dark:bg-zinc-900/40 bg-secondary/[0.01]"
           : "opacity-50 hover:opacity-100"
       } duration-300 py-2 px-3 dark:border-zinc-800 border-secondary/20 rounded-lg w-full text-center dark:text-white text-secondary text-sm font-medium`}
     >
--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function NavbarSection() {
   return (
-    <div className="flex flex-col py-8 items-center sticky top-0 bg-transparent backdrop-blur-sm z-[10]">
+    <div className="flex flex-col py-8 items-center sticky top-0 bg-primary/20 backdrop-blur-sm z-[10]">
       <div className="flex justify-between w-full max-w-screen-lg rounded-full px-12">
         <Link
           href="/"
@@ -21,7 +21,7 @@ export default function NavbarSection() {
           />
         </Link>
 
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4 bg-[#faf6e8] rounded-[12px] p-2 shadow shadow-secondary/15 dark:shadow-zinc-700/80 dark:bg-zinc-900/40">
           <NavbarItem href="/posts" text="Posts" />
           <NavbarItem href="/works" text="Works" />
           <NavbarItem href="/about" text="About" />
