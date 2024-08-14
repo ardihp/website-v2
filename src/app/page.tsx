1f9678e4d5a3f2795c544a531cae5c23413baa46
ardihp <ardihp8@gmail.com>
2024-08-14 16:38:52 +0700

    feat(home): index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import HomeView from "@/modules/home/view";
+import { fetchPages } from "@/lib/notion";
+import { getWebsiteMetrics } from "@/hooks/use-umami";
 
-export default function HomePage() {
-  return <div className="font-manrope">HomePage</div>;
+export default async function HomePage() {
+  const posts = await fetchPages();
+  const { pages } = await getWebsiteMetrics();
+
+  return <HomeView posts={posts?.results} pages={pages} />;
 }
