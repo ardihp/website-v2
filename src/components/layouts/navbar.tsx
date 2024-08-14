21a552b06a651c525a7a25d9d2e729b1ad27ac5c
ardihp <ardihp8@gmail.com>
2024-08-14 15:24:32 +0700

    fix(layouts): navbar & navbar item

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -1,35 +1,13 @@
 "use client";
 
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import NavbarItem from "./components/navbar-item";
 import Link from "next/link";
 
 export default function NavbarSection() {
-  const [showScroll, setShowScroll] = useState(false);
-
-  useEffect(() => {
-    document.addEventListener("scroll", () => {
-      if (scrollY >= 100) {
-        setShowScroll(true);
-      } else {
-        setShowScroll(false);
-      }
-    });
-
-    return () => {
-      document.removeEventListener("scroll", () => {});
-    };
-  }, []);
-
   return (
-    <header
-      className={`flex flex-col py-8 items-center sticky top-0 dark:bg-transparent bg-[#faf6e8]/20 backdrop-blur-sm z-[10] shadow ${
-        showScroll
-          ? "shadow-secondary/15 dark:shadow-zinc-700/80"
-          : "shadow-transparent"
-      } duration-200`}
-    >
+    <header className="flex flex-col py-8 items-center bg-transparent">
       <section className="flex justify-between w-full max-w-screen-lg rounded-full px-12">
         <Link
           href="/"
@@ -45,7 +23,7 @@ export default function NavbarSection() {
           />
         </Link>
 
-        <nav className="flex items-center gap-4 bg-[#faf6e8] rounded-[12px] p-2 shadow shadow-secondary/15 dark:shadow-zinc-700/80 dark:bg-zinc-900/40">
+        <nav className="flex items-center gap-4 bg-[#faf6e8] rounded-[12px] p-2 shadow dark:shadow-inner shadow-secondary/15 dark:shadow-zinc-700/80 dark:bg-zinc-900/40">
           <NavbarItem href="/posts" text="Posts" />
           <NavbarItem href="/works" text="Works" />
           <NavbarItem href="/about" text="About" />
