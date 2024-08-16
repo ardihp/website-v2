f47d1b12db7ac90553b6d446c6f1acc0c40fed5f
ardihp <ardihp8@gmail.com>
2024-08-16 23:09:03 +0700

    fix(data): experience

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -15,7 +15,7 @@ export const workExperience: WorkExperience[] = [
     role: "Frontend Engineer",
     start_date: "2022-09-22",
     end_date: null,
-    description: `My responsibilities include creating and optimizing landing page websites that align with the company's branding and user engagement goals. I also collaborate closely with the Backend team to implement and maintain RESTful API architecture, ensuring seamless integration and functionality across our platforms.`,
+    description: `Creating and optimizing landing page websites that align with the company's branding and user engagement.`,
     link: "https://hyppe.id",
   },
   {
@@ -24,7 +24,7 @@ export const workExperience: WorkExperience[] = [
     role: "Frontend Engineer",
     start_date: "2022-01-27",
     end_date: "2022-09-18",
-    description: `Expand the reach of Wispay to the general public through the landing page website. Helping the needs of internal admins to solve bugs in a product and work closely with the Backend team to implement RESTful API architecture during development or maintenance.`,
+    description: `Expand the reach of Wispay to the general public through the landing page website, also helping the needs of internal admins to solve bugs in a product.`,
     link: "https://wis-pay.com",
   },
   {
@@ -33,7 +33,7 @@ export const workExperience: WorkExperience[] = [
     role: "Frontend Engineer",
     start_date: "2021-09-01",
     end_date: "2021-12-01",
-    description: `I was responsible for developing a real-time monitoring and inventory management dashboard that streamlined operations across multiple platforms. This system enabled the capture and integration of critical data, including order status and customer historical purchases, ensuring accurate and efficient order management.`,
+    description: `Developing a real-time monitoring and inventory management dashboard.`,
     link: "https://www.instagram.com/lokapadang/",
   },
 ];
