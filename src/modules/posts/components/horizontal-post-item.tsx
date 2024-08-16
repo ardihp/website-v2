d8967c955e086675b9e629afd25cd38b0a639c98
ardihp <ardihp8@gmail.com>
2024-08-16 09:10:04 +0700

    fix(posts): component horizontal post item

--- src/modules/posts/components/horizontal-post-item.tsx
+++ src/modules/posts/components/horizontal-post-item.tsx
@@ -1,5 +1,5 @@
 import DelayedItem from "@/components/layouts/components/delayed-item";
-import { IconTimeline } from "@tabler/icons-react";
+import { IconArrowNarrowRight, IconTimeline } from "@tabler/icons-react";
 import dayjs from "dayjs";
 import Image from "next/image";
 import Link from "next/link";
@@ -20,13 +20,15 @@ export default function HorizontalPostItem({
   end,
   viewCount,
 }: PostItemsProps) {
+  console.log(post);
+
   return (
     <DelayedItem delay={delay} start={start} end={end}>
       <Link
         href={`/posts/${post?.properties?.slug?.rich_text?.[0]?.plain_text}`}
         passHref
       >
-        <div className="flex items-start gap-2 h-full w-full p-4 rounded-[24px] shadow-inner shadow-secondary/10 dark:shadow-zinc-700 dark:bg-zinc-900/40 bg-secondary/[0.02] scale-100 top-0 hover:top-[-6px] duration-200 active:top-0 active:scale-[0.99]">
+        <div className="flex items-start gap-2 h-full w-full p-4 group rounded-[24px] shadow-inner shadow-secondary/10 dark:shadow-zinc-700 dark:bg-zinc-900/40 bg-secondary/[0.01] scale-100 relative top-0 hover:top-[-6px] duration-200 active:top-0 active:scale-[0.99]">
           <div className="relative h-[140px] w-full max-w-[200px] rounded-[14px] overflow-hidden">
             <Image
               src={post?.cover?.file?.url || post?.cover?.external?.url}
@@ -40,22 +42,31 @@ export default function HorizontalPostItem({
             <div className="bg-black/30 w-full h-full absolute top-0" />
           </div>
 
-          <div className="flex flex-col gap-3 p-4 w-full">
+          <div className="flex flex-col gap-3 px-4 pt-2 w-full">
             <div className="flex gap-2 items-center">
-              <p className="text-xs text-secondary/90">
-                {dayjs(post?.created_time).format("DD MMM YYYY")}
+              <p className="text-xs font-medium text-secondary/70">
+                {dayjs(post?.properties?.created_at?.date?.start).format(
+                  "DD MMM YYYY"
+                )}
               </p>
               <IconTimeline size={16} className="text-secondary" />
-              <p className="text-xs text-secondary/90">{viewCount} views</p>
+              <p className="text-xs font-medium text-secondary/70">
+                {viewCount} views
+              </p>
             </div>
 
-            <div className="flex flex-col gap-2">
-              <p className="font-fredoka text-xl font-medium leading-5 text-pretty line-clamp-2">
+            <div className="flex flex-col gap-1">
+              <p className="font-fredoka text-xl font-medium leading-6 text-pretty line-clamp-2 opacity-80">
                 {post?.properties?.title?.title?.[0]?.plain_text}
               </p>
-              <p className="font-manrope text-sm font-medium text-pretty line-clamp-2 opacity-70">
+              <p className="font-manrope text-sm font-bold text-pretty line-clamp-2 opacity-60">
                 {post?.properties?.description?.rich_text?.[0]?.plain_text}
               </p>
+            </div>
+
+            <div className="flex items-center gap-2 group-hover:gap-3 duration-200">
+              <p className="text-sm font-medium opacity-80">Read More</p>
+              <IconArrowNarrowRight size={20} className="text-secondary/70 mt-[2px]" />
             </div>
           </div>
         </div>
