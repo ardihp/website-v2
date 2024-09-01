943caae939237c8a9afab68cfd598184b081a986
ardihp <ardihp8@gmail.com>
2024-09-01 14:12:55 +0700

    feat: next config image remote hostname

--- next.config.mjs
+++ next.config.mjs
@@ -21,17 +21,18 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "www.notion.so",
-        port: "",
       },
       {
         protocol: "https",
         hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
-        port: "",
       },
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
       },
     ],
   },
