194e56fb096f103964944ea2f1467d5462f5e5cf
ardihp <ardihp8@gmail.com>
2024-09-08 14:31:38 +0700

    fix(layouts): border color style

--- src/modules/about/components/biography.tsx
+++ src/modules/about/components/biography.tsx
@@ -6,7 +6,7 @@ export default function BiographySection() {
   return (
     <article className="flex flex-col items-center gap-4 mt-10 md:mt-12 border-2 border-dashed border-secondary/20 dark:border-zinc-700/60 rounded-[20px] md:rounded-[32px] p-6 pt-8 md:p-10 md:pt-14 lg:p-16 lg:pt-20 relative shadow-inner dark:shadow-none shadow-secondary/10 dark:shadow-zinc-700">
       <div
-        className="absolute top-[-28px] md:top-[-40px] left-[26px] md:left-[32px] lg:left-[54px] w-fit md:w-auto p-2 md:p-4 px-3 md:px-6 bg-primary dark:bg-[#151515] bg-[url('/bg-noise.png')] bg-blend-exclusion dark:bg-blend-difference"
+        className="absolute top-[-28px] md:top-[-40px] left-[26px] md:left-[32px] lg:left-[54px] w-fit md:w-auto p-2 md:p-4 px-3 md:px-6 bg-primary dark:bg-[#121212] bg-[url('/bg-noise.png')] bg-blend-exclusion dark:bg-blend-normal"
         style={{ backgroundSize: "110px" }}
       >
         <div className="p-3 md:p-4 rounded-[8px] md:rounded-[14px] w-fit h-fit shadow-inner shadow-secondary/10 dark:shadow-zinc-700 dark:bg-zinc-900/40 bg-secondary/[0.2]">
--- src/modules/home/components/recent-posts.tsx
+++ src/modules/home/components/recent-posts.tsx
@@ -33,8 +33,8 @@ export default function RecentPostSection({
   return (
     <article className="flex flex-col items-center gap-4 border-2 border-dashed border-secondary/20 dark:border-zinc-700/60 rounded-[20px] md:rounded-[32px] p-6 pt-10 md:p-10 md:pt-14 lg:p-16 relative shadow-inner dark:shadow-none shadow-secondary/10 dark:shadow-zinc-700">
       <div
-        className="absolute top-[-28px] w-[calc(100%_-_108px)] flex items-center justify-between mx-auto md:top-[-40px] left-[26px] md:left-[32px] lg:left-[54px] p-2 md:p-4 px-3 md:px-6 bg-primary dark:bg-[#151515] bg-[url('/bg-noise.png')] bg-blend-exclusion dark:bg-blend-difference"
-        style={{ backgroundSize: "110px" }}
+        className="absolute top-[-28px] w-[calc(100%_-_108px)] flex items-center justify-between mx-auto md:top-[-40px] left-[26px] md:left-[32px] lg:left-[54px] p-2 md:p-4 px-3 md:px-6 bg-primary dark:bg-[#121212] bg-[url('/bg-noise.png')] bg-blend-exclusion dark:bg-blend-normal"
+        style={{ backgroundSize: "100px" }}
       >
         <div className="p-3 md:p-4 rounded-[8px] md:rounded-[14px] w-fit h-fit shadow-inner shadow-secondary/10 dark:shadow-zinc-700 dark:bg-zinc-900/40 bg-secondary/[0.2]">
           <p className="shadowed-text text-primary text-[16px] md:text-[20px] font-[600]">
