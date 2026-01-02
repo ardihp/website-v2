ffa43b973077fd428c28d580ff14f00405db02af
ardihp <ardihp8@gmail.com>
2026-01-02 10:17:11 +0700

    fix(components): implement new image kit deps

--- src/components/layouts/components/imagekit.tsx
+++ src/components/layouts/components/imagekit.tsx
@@ -1,17 +1,10 @@
 "use client";
 
 import React, { ComponentProps } from "react";
-import { IKImage } from "imagekitio-next";
+import { IKImageProps, Image } from "@imagekit/next";
 
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 
-export default function ImageKit(props: ComponentProps<any>) {
-  return (
-    <IKImage
-      urlEndpoint={urlEndpoint}
-      lqip={{ active: true, quality: 10 }}
-      loading="lazy"
-      {...props}
-    />
-  );
+export default function ImageKit(props: IKImageProps) {
+  return <Image urlEndpoint={urlEndpoint} loading="lazy" {...props} />;
 }
