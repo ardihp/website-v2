964f2d87e52afd3e230373c16172147642dadd9f
ardihp <ardihp8@gmail.com>
2026-01-05 11:12:00 +0700

    fix(components): section experience in about page

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -4,6 +4,8 @@ export interface WorkExperience {
   role: string;
   start_date: string;
   end_date: string | null;
+  location: String;
+  work_type: String;
   description: string;
   job_list: string[];
   link?: string;
@@ -16,7 +18,10 @@ export const workExperience: WorkExperience[] = [
     role: "Frontend Engineer",
     start_date: "2023-01-01",
     end_date: null,
-    description: `Developed and designed web mockup with cross-browser compatibility for company profile website, complete with selling features to sell more than 100+ products for international sales.`,
+    location: "Jakarta",
+    work_type: "Remote",
+    description:
+      "As a freelancer, I have collaborated with various clients to deliver high-quality web applications and digital solutions tailored to their specific needs.",
     job_list: [
       "Developing and launching a complex web-based marketplace platform for international clients, supporting efficient and scalable e-commerce operations.",
       "Developing and implementing a comprehensive internal CRM dashboard, integrating invoice management, attendance, and real-time live chat features (using Socket.IO) to enhance operational efficiency and team communication.",
@@ -29,7 +34,10 @@ export const workExperience: WorkExperience[] = [
     role: "Frontend Developer",
     start_date: "2025-07-27",
     end_date: null,
-    description: ``,
+    location: "Jakarta",
+    work_type: "On-site",
+    description:
+      "PT Steradian Data Optima is a trusted technology partner that delivers smart, secure, and scalable digital solutions to support long-term organizational growth.",
     job_list: [
       "Working as Frontend Developer at PT Bank Rakyat Indonesia to develop the Payroll module for QLola (Top 5 feature), focusing on delivering a high-quality and intuitive user experience.",
       "Collaborated with the product team to design and deploy new payroll capabilities tailored to user needs.",
@@ -45,7 +53,10 @@ export const workExperience: WorkExperience[] = [
     role: "Frontend Developer",
     start_date: "2024-09-09",
     end_date: "2025-07-27",
-    description: ``,
+    location: "Jakarta",
+    work_type: "On-site",
+    description:
+      "PT. Saudara Digital Indonesia or as known as Peha Agency is a digital agency company that giving an ditial solution for business needs. We are specialize in Social Media Management and Digital Advertising.",
     job_list: [
       "Developing a responsive company web profile to enhance the company's digital image and online presence.",
       "Creating an internal invoice and quotation management application that reduces document creation time and minimizes manual errors.",
@@ -60,7 +71,10 @@ export const workExperience: WorkExperience[] = [
     role: "Frontend Engineer",
     start_date: "2022-09-22",
     end_date: "2025-02-28",
-    description: `Creating and optimizing landing page websites that align with the company's branding and user engagement.`,
+    location: "Jakarta",
+    work_type: "On-site",
+    description:
+      'PT. Hyppe Teknologi Indonesia is the company behind "Hyppe" application, a content management platform that helps content creators to manage and distribute their digital content easily.',
     job_list: [
       "Managing and developing new features on the internal operational dashboard, supporting mobile application needs, and ensuring seamless integration with Alibaba VOD (Video on Demand) for efficient content management.",
       "Responsible for the maintenance and development of new features on the dashboard accessed directly by application users, with a focus on enhancing user engagement and satisfaction.",
@@ -74,7 +88,10 @@ export const workExperience: WorkExperience[] = [
     role: "Frontend Engineer",
     start_date: "2022-01-27",
     end_date: "2022-09-18",
-    description: `Expand the reach of Wispay to the general public through the landing page website, also helping the needs of internal admins to solve bugs in a product.`,
+    location: "Jakarta",
+    work_type: "Remote",
+    description:
+      "PT. Bintang Digital Asia is or known as Wispay, a financial technology company that provides digital payment solutions for businesses and individuals in Indonesia.",
     job_list: [
       "Expand the reach of Wispay to the general public through the landing page website.",
       "Helping the needs of internal admins to solve bugs in a product and work closely with the Backend team to implement RESTful API architecture during development or maintenance.",
@@ -87,7 +104,9 @@ export const workExperience: WorkExperience[] = [
     role: "Frontend Engineer",
     start_date: "2021-09-01",
     end_date: "2021-12-01",
-    description: `Developing a real-time monitoring and inventory management dashboard.`,
+    location: "Jakarta",
+    work_type: "Remote",
+    description: `PT. Loka Padang is a culinary business that specializes in Vegetarian Padang food, offering a variety of traditional Padang dishes made from plant-based ingredients.`,
     job_list: [
       "Create real time monitoring and inventory management dashboard. Customer enquiry and orders flow in from various sources at the same time in multiple platforms, two informations needs to be captured (order status, customer historical purchases) from this data management system",
     ],
--- src/modules/about/components/experience.tsx
+++ src/modules/about/components/experience.tsx
@@ -21,48 +21,61 @@ export default function ExperienceSection() {
 
       <div className="flex flex-col gap-8 w-full">
         {workExperience?.map((work: WorkExperience, index: number) => (
-          <Link
-            key={index}
-            href={work?.link ? work?.link : "/works"}
-            target={work?.link ? "_blank" : "_self"}
-            passHref
+          <div
+            key={work.company}
+            className="flex flex-col md:flex-row gap-3 md:gap-6"
           >
-            <div className="flex flex-col md:flex-row gap-3 md:gap-6">
-              <div className="flex items-center justify-center w-full h-[72px] md:h-[80px] max-w-[72px] md:max-w-[80px] rounded-[16px] md:rounded-[20px] shadow-inner shadow-secondary/10 dark:shadow-zinc-700 dark:bg-zinc-900/40 bg-secondary/[0.01]">
-                <div className="relative size-[48px] md:size-[56px] rounded-[12px] overflow-hidden shadow-lg shadow-secondary/10 dark:shadow-zinc-700">
-                  <Image
-                    src={work?.logo}
-                    alt="Company Logo"
-                    fill
-                    sizes="200px"
-                    className="object-cover object-center"
-                    priority
-                  />
-                </div>
+            <Link
+              key={index}
+              href={work?.link ? work?.link : "/works"}
+              target={work?.link ? "_blank" : "_self"}
+              className="flex items-center w-full h-[72px] md:h-[80px] max-w-[72px] md:max-w-[80px] justify-center rounded-[16px] md:rounded-[20px] shadow-inner shadow-secondary/10 dark:shadow-zinc-700 dark:bg-zinc-900/40 bg-[#f5f0e2] z-10"
+              passHref
+            >
+              <div className="relative size-[48px] md:size-[56px] rounded-[12px] overflow-hidden shadow-lg shadow-secondary/10 dark:shadow-zinc-700">
+                <Image
+                  src={work?.logo}
+                  alt="Company Logo"
+                  fill
+                  sizes="200px"
+                  className="object-cover object-center"
+                  priority
+                />
               </div>
+            </Link>
 
-              <div className="flex flex-col md:p-2 w-full">
-                <div className="flex flex-col md:flex-row md:items-center justify-between">
-                  <p className="font-manrope font-black text-base md:text-md text-secondary/70 dark:text-white">
-                    {work.company}
-                  </p>
-
-                  <p className="text-xs md:text-sm font-medium text-secondary/50 dark:text-white/70 mb-2 md:mb-0">
-                    {dayjs(work.start_date).format("MMM YYYY")} -{" "}
-                    {work?.end_date
-                      ? dayjs(work?.end_date).format("MMM YYYY")
-                      : "Present"}
-                  </p>
-                </div>
-                <p className="text-base font-medium text-secondary/50 dark:text-white/90">
+            <div className="flex flex-col md:p-2 w-full">
+              <div className="flex flex-col md:flex-row md:items-center justify-between">
+                <p className="font-manrope font-black text-base md:text-md text-secondary/70 dark:text-white">
                   {work.role}
                 </p>
-                <p className="font-manrope text-[10px] md:text-xs font-bold mt-1 md:mt-2 text-secondary/40 dark:text-white/70 text-pretty">
-                  {work.description}
+
+                <p className="font-manrope text-[10px] md:text-xs font-bold text-secondary/50 dark:text-white/70 mb-2 md:mb-0">
+                  {dayjs(work.start_date).format("MMM YYYY")} -{" "}
+                  {work?.end_date
+                    ? dayjs(work?.end_date).format("MMM YYYY")
+                    : "Current"}
                 </p>
               </div>
+              <p className="font-manrope text-xs font-bold text-secondary/50 dark:text-white/90 mt-0.5">
+                {work.company} -- {work.location}, {work.work_type}
+              </p>
+              <p className="font-manrope text-[10px] md:text-xs font-bold mt-1 md:mt-3 text-secondary/40 dark:text-white/70 text-pretty">
+                {work.description}
+              </p>
+
+              <ul className="list-disc pl-5">
+                {work.job_list.map((job, jobIdx) => (
+                  <li
+                    key={jobIdx}
+                    className="font-manrope text-[10px] md:text-xs font-bold mt-1 md:mt-3 text-secondary/40 dark:text-white/70 text-pretty"
+                  >
+                    {job}
+                  </li>
+                ))}
+              </ul>
             </div>
-          </Link>
+          </div>
         ))}
       </div>
     </article>
