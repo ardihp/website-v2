4522e8fa75a4bd795556cb304419b5451c155b0e
ardihp <ardihp8@gmail.com>
2026-01-05 11:12:18 +0700

    fix(components): navbar item

--- src/components/layouts/components/navbar-item.tsx
+++ src/components/layouts/components/navbar-item.tsx
@@ -18,9 +18,9 @@ export default function NavbarItem({ href, text, ...rest }: NavbarItemProps) {
       {...rest}
       className={`${
         pathname.match(href)
-          ? "opacity-100 shadow-inner dark:shadow shadow-secondary/15 dark:shadow-zinc-700/80 dark:bg-zinc-900/40 bg-secondary/[0.01]"
-          : "opacity-70 hover:opacity-100"
-      } duration-300 py-2 px-3 dark:border-zinc-800 border-secondary/20 rounded-lg w-full text-center dark:text-white text-secondary text-xs md:text-sm font-medium`}
+          ? "opacity-100 shadow-inner dark:shadow"
+          : "opacity-70 hover:opacity-100 active:shadow-inner dark:active:shadow"
+      } duration-300 py-2 px-3 dark:border-zinc-800 border-secondary/20 shadow-secondary/15 dark:shadow-zinc-700/80 dark:bg-zinc-900/40 bg-secondary/[0.01] rounded-lg w-full text-center dark:text-white text-secondary text-xs md:text-sm font-medium`}
     >
       {text}
     </Link>
