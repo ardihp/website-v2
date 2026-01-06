2cd111d146458cced6445b98b5f2b71a0d99c9e0
ardihp <ardihp8@gmail.com>
2026-01-06 16:38:21 +0700

    fix(posts): dummy detail page

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,11 +1,7 @@
 import React from "react";
-import { fetchBySlug, fetchPageBlocks, fetchPages, notion } from "@/lib/notion";
-import { notFound } from "next/navigation";
-import { NotionRenderer } from "@notion-render/client";
-import hljsPlugin from "@notion-render/hljs-plugin";
-import bookmarkPlugin from "@notion-render/bookmark-plugin";
-import PostBySlugView from "@/modules/posts/slug/view";
 import { getWebsiteMetrics } from "@/hooks/use-umami";
+import { getPosts } from "@/lib/posts";
+import { PostItemProps } from "@/modules/posts/view";
 
 interface BlogSlugPageProps {
   slug: string;
@@ -14,10 +10,10 @@ interface BlogSlugPageProps {
 export const dynamicParams = false;
 
 export async function generateStaticParams() {
-  const posts = await fetchPages();
+  const posts = await getPosts();
 
-  return posts.results.map((post: any) => ({
-    slug: post?.properties?.slug?.rich_text?.[0]?.plain_text,
+  return posts.map((post: PostItemProps) => ({
+    slug: post.slug,
   }));
 }
 
@@ -27,23 +23,12 @@ export default async function BlogSlugPage({
   params: Promise<BlogSlugPageProps>;
 }) {
   const { slug } = await params;
-  const post: any = await fetchBySlug(slug);
   const { pages } = await getWebsiteMetrics();
 
-  if (!post) {
-    notFound();
-  }
-
-  const blocks = await fetchPageBlocks(post.id);
-
-  const renderer = new NotionRenderer({
-    client: notion,
-  });
-
-  renderer.use(hljsPlugin({}));
-  renderer.use(bookmarkPlugin(undefined));
-
-  const html = await renderer.render(...blocks);
-
-  return <PostBySlugView post={post} html={html} pages={pages} />;
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>This is a sample MDX content for the post with slug: {slug}</p>
+    </div>
+  );
 }
