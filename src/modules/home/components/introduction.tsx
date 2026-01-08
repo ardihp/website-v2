1f0827c80385910f56aac0095f1ba7891d64b485
ardihp <ardihp8@gmail.com>
2026-01-08 16:39:08 +0700

    fix(home): content introduction section

--- src/modules/home/components/introduction.tsx
+++ src/modules/home/components/introduction.tsx
@@ -33,7 +33,7 @@ export default function IntroductionSection() {
       </section>
 
       <p className="font-medium text-base md:text-lg w-full md:w-[550px] mr-auto text-pretty text-secondary/60 dark:text-white/70">
-        Frontend Developer with over two years of experience in crafting
+        Frontend Developer with over three years of experience in crafting
         responsive and visually appealing websites.
       </p>
     </article>
