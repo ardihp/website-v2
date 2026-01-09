ff20977fdff5498a8cc09df89490d35d7eb1c15f
ardihp <ardihp8@gmail.com>
2026-01-09 09:16:36 +0700

    fix(posts): add post tags to card

--- src/modules/posts/components/post-item.tsx
+++ src/modules/posts/components/post-item.tsx
@@ -26,6 +26,17 @@ export default function PostItem({ post, viewCount }: PostItemsProps) {
         </div>
 
         <div className="absolute bottom-6 w-[calc(100%_-_48px)] flex flex-col gap-2">
+          <div className="flex items-center gap-2">
+            {post.body.tags.split(",").map((tag, number) => (
+              <div
+                key={number}
+                className="flex gap-1 items-center bg-primary/5 py-[5px] px-[10px] backdrop-blur-sm border border-primary/10 rounded-[16px] w-fit"
+              >
+                <p className="text-xs text-primary">{tag.trim()}</p>
+              </div>
+            ))}
+          </div>
+
           <div className="flex flex-col gap-2 p-3 bg-primary/5 backdrop-blur-sm border border-primary/10 rounded-[16px]">
             <p className="font-fredoka text-lg font-medium leading-5 text-pretty line-clamp-2 text-primary">
               {post.body.title}
@@ -37,7 +48,7 @@ export default function PostItem({ post, viewCount }: PostItemsProps) {
         </div>
 
         <div className="absolute top-6 right-6 flex gap-1 items-center bg-primary/5 py-[5px] px-[10px] backdrop-blur-sm border border-primary/10 rounded-[16px] w-fit">
-          <p className="text-xs text-primary/90">
+          <p className="text-xs text-primary">
             {dayjs(post.body.publishedOn).format("MMM D, YYYY")}
           </p>
           <IconPointFilled size={12} className="text-primary" />
