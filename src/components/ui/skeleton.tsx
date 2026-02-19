ee13704e51aae18dc04ffb6e5787ba6862ac4354
Ardi <ardihp8@gmail.com>
2026-02-19 10:25:36 +0700

    feat(components): shadcn skeleton

---
+++ src/components/ui/skeleton.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        "animate-pulse rounded-md bg-secondary/5 dark:bg-zinc-50/10",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
