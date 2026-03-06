7162491c059c884ce019af4fb2c8e84eb54b4131
Ardi <ardihp8@gmail.com>
2026-03-06 11:04:50 +0700

    ffix(components): content spotify section footer

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -9,16 +9,22 @@ import {
   IconBrandLinkedin,
   IconBrandNextjs,
   IconBrandRadixUi,
+  IconBrandSpotify,
   IconBrandSpotifyFilled,
   IconBrandTailwind,
   IconBrandVercel,
   IconLetterCSmall,
   IconLetterVSmall,
+  IconLink,
+  IconSocial,
 } from "@tabler/icons-react";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
+import { menuItems } from "./navbar";
+import DelayedItem from "./components/delayed-item";
+import { Skeleton } from "../ui/skeleton";
 
 function ContactItem({ icon, link }: { icon: ReactNode; link: string }) {
   return (
@@ -42,32 +48,35 @@ interface SongItem {
 export default function FooterSection() {
   const pathname = usePathname();
   const [song, setSong] = useState<SongItem | null>(null);
+  const [loadingSong, setLoadingSong] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchNowPlaying = async () => {
       const res = await fetch("/api/spotify");
       const data = await res.json();
       if (!data.error) setSong(data);
+      setLoadingSong(false);
     };
     fetchNowPlaying();
 
-    // Optional: refresh every 30 seconds
-    const interval = setInterval(fetchNowPlaying, 30000);
+    // Optional: refresh every 60 seconds
+    const interval = setInterval(fetchNowPlaying, 60000);
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <footer className="w-full py-6 md:py-16">
+    <footer className="w-full py-6 md:py-12 xl:py-16">
       <section
         className={cn(
-          "w-full max-w-screen-lg mx-auto",
+          "w-full max-w-screen-lg mx-auto duration-500",
           pathname.includes("/posts/")
-            ? "px-4 md:px-6 xl:px-0"
-            : "px-4 md:px-8 xl:px-12",
+            ? "px-4 md:px-6"
+            : "px-4 md:px-8",
         )}
       >
-        <div className="w-full grid grid-cols-[repeat(8,1fr)] border-2 border-secondary/20 border-dashed rounded-[32px] shadow-inner">
-          <div className="col-span-2 row-span-3 flex items-center justify-center">
+        <div className="w-full grid grid-cols-[repeat(4,1fr)] lg:grid-cols-[repeat(8,1fr)] border-2 border-secondary/20 border-dashed rounded-[32px] shadow-inner overflow-hidden">
+          {/* Logo */}
+          <div className="lg:col-span-2 lg:row-span-3 flex items-center justify-center">
             <Link href="/">
               <Image
                 src="/logo.png"
@@ -80,109 +89,119 @@ export default function FooterSection() {
               />
             </Link>
           </div>
-          <div className="col-span-3 border-2 border-secondary/20 border-dashed border-t-0 flex gap-6 items-center justify-center h-20">
+
+          {/* Link pages */}
+          <div className="col-span-3 lg:border-2 border-secondary/20 border-dashed lg:border-t-0 flex gap-12 items-center justify-center h-20">
+            {menuItems.map((link) => (
+              <Link
+                href={link.href}
+                key={link.label}
+                className="text-sm font-medium text-secondary/40 hover:text-secondary duration-200"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </div>
+
+          {/* Spotify */}
+          <div className="col-span-4 lg:col-span-3 border-t-2 lg:border-b-2 lg:border-t-0 border-secondary/20 border-dashed h-20 flex gap-6 items-center justify-center px-6 relative overflow-hidden">
+            <IconBrandSpotify className="size-14 absolute -bottom-4 -right-4 rotate-[20deg] text-secondary/5" />
+
+            <div className="w-full flex flex-col gap-0.5">
+              <div className="flex items-center justify-between">
+                {loadingSong ? (
+                  <Skeleton className="w-40 h-6" />
+                ) : (
+                  <DelayedItem start="bottom" end="top" delay={0.25}>
+                    <Link
+                      href={song?.songUrl || "#"}
+                      className="w-fit -mt-1"
+                      target="_blank"
+                      referrerPolicy="no-referrer"
+                      title={song?.title}
+                      passHref
+                    >
+                      <p className="font-semibold text-secondary/60 capitalize hover:text-secondary/40 duration-200 truncate w-[220px]">
+                        {song?.title}
+                      </p>
+                    </Link>
+                  </DelayedItem>
+                )}
+
+                <div className="flex items-center gap-2">
+                  <div className="flex gap-0.5 items-center">
+                    <div className="h-[10px] w-0.5 bg-secondary/40 rounded-full animate-music-audio" />
+                    <div className="h-[10px] w-0.5 bg-secondary/40 rounded-full animate-music-audio delay-200" />
+                    <div className="h-[10px] w-0.5 bg-secondary/40 rounded-full animate-music-audio delay-500" />
+                    <div className="h-[10px] w-0.5 bg-secondary/40 rounded-full animate-music-audio delay-75" />
+                  </div>
+                  <p className="text-sm text-secondary/40 font-medium">
+                    {song?.isPlaying ? "Online" : "Offline"}
+                  </p>
+                </div>
+              </div>
+
+              {loadingSong ? (
+                <Skeleton className="w-20 h-4" />
+              ) : (
+                <DelayedItem start="bottom" end="top" delay={0.4}>
+                  <p className="font-manrope font-bold text-xs text-secondary/40 capitalize truncate w-[250px]">
+                    {song?.artist}
+                  </p>
+                </DelayedItem>
+              )}
+            </div>
+          </div>
+
+          {/* Social Media */}
+          <div className="col-span-4 lg:col-span-2 border-y-2 lg:border-x-2 lg:border-y-0 border-secondary/20 border-dashed flex gap-8 lg:gap-5 justify-center items-center h-20 p-4 relative overflow-hidden">
+            <IconLink className="size-14 absolute -bottom-4 -right-4 lg:-top-4 lg:-right-4 -rotate-[90deg] text-secondary/5" />
             <ContactItem
               icon={
-                <IconBrandGmail className="text-secondary size-4 md:size-5" />
+                <IconBrandGmail className="text-secondary size-6 lg:size-5" />
               }
               link="mailto:ardihp8@gmail.com"
             />
             <ContactItem
               icon={
-                <IconBrandInstagram className="text-secondary size-4 md:size-5" />
+                <IconBrandInstagram className="text-secondary size-6 lg:size-5" />
               }
               link="https://www.instagram.com/ardi.hp/"
             />
             <ContactItem
               icon={
-                <IconBrandGithub className="text-secondary size-4 md:size-5" />
+                <IconBrandGithub className="text-secondary size-6 lg:size-5" />
               }
               link="https://github.com/ardihp"
             />
             <ContactItem
               icon={
-                <IconBrandLinkedin className="text-secondary size-4 md:size-5" />
+                <IconBrandLinkedin className="text-secondary size-6 lg:size-5" />
               }
               link="https://www.linkedin.com/in/ardi-hp"
             />
             <ContactItem
               icon={
                 <div className="flex items-center gap-0">
-                  <IconLetterCSmall className="text-secondary ml-[-4px] h-[20px] w-[20px] md:h-[24px] md:w-[24px]" />
-                  <IconLetterVSmall className="text-secondary ml-[-14px] md:ml-[-16px] h-[20px] w-[20px] md:h-[24px] md:w-[24px]" />
+                  <IconLetterCSmall className="text-secondary ml-[-10px] lg:ml-[-4px] size-8 lg:size-6" />
+                  <IconLetterVSmall className="text-secondary ml-[-20px] lg:ml-[-14px] md:ml-[-16px] size-8 lg:size-6" />
                 </div>
               }
               link="https://drive.google.com/file/d/1W771KkKruTaHKY2PT_Z3NiGYe8J5gyCu/view?usp=sharing"
             />
           </div>
-          <div className="col-span-3 border-b-2 border-secondary/20 border-dashed h-20 flex gap-6 items-center justify-center px-6">
-            {/* <div className="flex gap-0.5 items-center">
-              <div className="h-5 w-1 bg-secondary/40 rounded-full" />
-              <div className="h-5 w-1 bg-secondary/40 rounded-full" />
-              <div className="h-5 w-1 bg-secondary/40 rounded-full" />
-              <div className="h-5 w-1 bg-secondary/40 rounded-full" />
-            </div> */}
-            <div className="w-full flex flex-col gap-0.5">
-              <Link
-                href={song?.songUrl || "#"}
-                className="w-fit -mt-1"
-                target="_blank"
-                referrerPolicy="no-referrer"
-                passHref
-              >
-                <p className="font-semibold text-secondary/60 capitalize hover:text-secondary/40 duration-200 text-lg truncate w-[250px]">
-                  {song?.title}
-                </p>
-              </Link>
-              <p className="font-manrope font-bold text-sm text-secondary/40 capitalize truncate w-[250px]">
-                {song?.artist}
-              </p>
-            </div>
-          </div>
-          <div className="col-span-2 border-x-2 border-secondary/20 border-dashed flex flex-col items-center justify-center h-20">
+
+          {/* Made with */}
+          <div className="col-span-2 lg:border-r-2 border-secondary/20 border-dashed hidden lg:flex flex-col items-center justify-center h-20">
             <p className="text-secondary/60 font-medium text-sm">
               Made with ❤️ & 🥴
             </p>
           </div>
-          <div className="col-span-2 border-r-2 border-secondary/20 border-dashed flex gap-3 justify-center items-center h-20 p-4">
-            <p className="text-secondary/60 font-medium text-sm">Tech used</p>
-            <div className="flex items-center gap-1 rounded-full">
-              <FooterTechItem
-                href="https://nextjs.org/"
-                title="Next.js"
-                icon={
-                  <IconBrandNextjs className="text-secondary dark:text-white size-4 md:size-[18px]" />
-                }
-              />
-              <FooterTechItem
-                href="https://tailwindcss.com/"
-                title="Tailwind Css"
-                icon={
-                  <IconBrandTailwind className="text-secondary dark:text-white size-4 md:size-[18px]" />
-                }
-              />
-              <FooterTechItem
-                href="https://ui.shadcn.com/"
-                title="Radix with Shadcn/ui"
-                icon={
-                  <IconBrandRadixUi className="text-secondary dark:text-white  size-4 md:size-[18px]" />
-                }
-              />
-              <FooterTechItem
-                href="https://vercel.com/home"
-                title="Vercel"
-                icon={
-                  <IconBrandVercel
-                    fill="#470A00"
-                    className="text-secondary dark:text-white size-4 md:size-[18px]"
-                  />
-                }
-              />
-            </div>
-          </div>
-          <div className="col-span-2 flex items-center justify-center h-20">
+
+          {/* Copyright */}
+          <div className="col-span-4 lg:col-span-2 flex items-center justify-center h-20">
             <p className="text-secondary/60 font-medium text-sm">
-              © {new Date().getFullYear()} ardihp
+              Copyright © {new Date().getFullYear()} ardihp
             </p>
           </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,18 +28,10 @@ const config = {
         tertiary: "#694500",
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+        "music-audio": { from: { height: "4px" }, to: { height: "10px" } },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        "music-audio": "music-audio 0.5s ease-in-out infinite alternate",
       },
     },
   },
